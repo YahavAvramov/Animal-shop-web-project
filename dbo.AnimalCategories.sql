495d0c6b1c279f73e93ad0c0a2e637963e6cb3e9
Yahav <109096266+YahavAvramov@users.noreply.github.com>
2022-08-24 16:07:06 +0300

    home page changes

---
+++ dbo.AnimalCategories.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[AnimalCategories] (
+    [CategoryID]    INT   NOT NULL,
+    [Name]          TEXT  NOT NULL,
+    [AnimalID]      INT   NOT NULL,
+    [CategoryImage] IMAGE NOT NULL
+);
+
