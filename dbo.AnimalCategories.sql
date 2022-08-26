01e4c775b72508673662cb04a702d688caafb5d1
Yahav <109096266+YahavAvramov@users.noreply.github.com>
2022-08-26 12:50:21 +0300

    Delete dbo.AnimalCategories.sql

--- dbo.AnimalCategories.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[AnimalCategories] (
-    [CategoryID]    INT   NOT NULL,
-    [Name]          TEXT  NOT NULL,
-    [AnimalID]      INT   NOT NULL,
-    [CategoryImage] IMAGE NOT NULL
-);
-
