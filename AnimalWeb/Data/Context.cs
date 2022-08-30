d067daefd3a437eacddcc4acda0907b3b4f81f0a
Maxim Senik <senik0maxim@gmail.com>
2022-08-30 13:39:19 +0300

    Added admin tables to context

--- AnimalWeb/Data/Context.cs
+++ AnimalWeb/Data/Context.cs
@@ -15,6 +15,9 @@ namespace AnimalWeb.Data
         public DbSet<Categories> Categories { get; set; }
         public DbSet<Animals> Animals { get; set; }
         public DbSet<Comments> Comments { get; set; }
+        //public DbSet<Users> Users { get; set; }
+        //public DbSet<UserAdmin> Admins { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -23,16 +26,27 @@ namespace AnimalWeb.Data
                 new { ID = 2, Name = "Cats", CategoryPicture = @"\Assets\CategoriesI-icons\cat-icon.webp" },
                 new { ID = 3, Name = "Fish", CategoryPicture = @"\Assets\CategoriesI-icons\fish-icon.webp" },
                 new { ID = 4, Name = "Rabbits", CategoryPicture = @"\Assets\CategoriesI-icons\rabbit-icon.jpg" },
-                new { ID = 5, Name = "Iguwanas", CategoryPicture = @"\Assets\CategoriesI-icons\iguana-icon.jpg" }
+                new { ID = 5, Name = "Iguanas", CategoryPicture = @"\Assets\CategoriesI-icons\iguana-icon.jpg" }
             );
             modelBuilder.Entity<Animals>().HasData(
                 new { ID = 1, Name = "Rex", Age = 1, PictureName = "Path", Description = "lorem50", CategoryID = 1, Price = 10},
-                new { ID = 2, Name = "Miki", Age = 2, PictureName = "Path", Description = "lorem50", CategoryID = 1, Price = 10},
-                new { ID = 3, Name = "Hatul", Age = 3, PictureName = "Path", Description = "lorem50", CategoryID = 2, Price = 10}
+                new { ID = 2, Name = "Miki", Age = 2, PictureName = "Path", Description = "lorem50", CategoryID = 1, Price = 23},
+                new { ID = 3, Name = "Hatul", Age = 3, PictureName = "Path", Description = "lorem50", CategoryID = 2, Price = 11},
+                new { ID = 4, Name = "Nemo", Age = 3, PictureName = "Path", Description = "lorem50", CategoryID = 3, Price = 22 },
+                new { ID = 5, Name = "Bunni", Age = 3, PictureName = "Path", Description = "lorem50", CategoryID = 4, Price = 2 },
+                new { ID = 6, Name = "Baks", Age = 3, PictureName = "Path", Description = "lorem50", CategoryID = 4, Price = 13 },
+                new { ID = 7, Name = "Ziki", Age = 3, PictureName = "Path", Description = "lorem50", CategoryID = 5, Price = 15 },
+                new { ID = 8, Name = "Ziko", Age = 3, PictureName = "Path", Description = "lorem50", CategoryID = 5, Price = 10 }
             );  
             modelBuilder.Entity<Comments>().HasData(
                 new { ID = 1, Comment = "Dogs", AnimalID = 1}
             );
+            //modelBuilder.Entity<Users>().HasData(
+            //    new { ID = 1, Comment = "Dogs", AnimalID = 1 }
+            //);
+            //modelBuilder.Entity<UserAdmin>().HasData(
+            //    new { ID = 1, Comment = "Dogs", AnimalID = 1 }
+            //);
 
         }
 
