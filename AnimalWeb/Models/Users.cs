9be23823da2fbd48d854cec05740c118884bef22
Maxim Senik <senik0maxim@gmail.com>
2022-08-30 13:38:34 +0300

    Added Admin models

---
+++ AnimalWeb/Models/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace AnimalWeb.Models
+{
+    public class Users
+    {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+
+    }
+}
