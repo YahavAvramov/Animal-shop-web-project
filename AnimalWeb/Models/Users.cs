3186f18814b1a7e73b80c04d7cca42bd10604a9a
Maxim Senik <senik0maxim@gmail.com>
2022-09-06 20:41:06 +0300

    Started working on auth API

--- AnimalWeb/Models/Users.cs
+++ AnimalWeb/Models/Users.cs
@@ -1,10 +1,18 @@
-﻿namespace AnimalWeb.Models
+﻿using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimalWeb.Models
 {
     public class Users
     {
         public int ID { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+        //public IList<AuthenticationScheme>? ExternalLogins { get; set; }
 
     }
 }
