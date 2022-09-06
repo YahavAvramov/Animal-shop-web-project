963f64608746f42a93d6681adb8c43587fb443f2
Maxim Senik <senik0maxim@gmail.com>
2022-09-06 21:57:04 +0300

    Added unit test, and all OK

---
+++ AnimalWeb.Tests/Controllers/CategoriesControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using AnimalWeb.Controllers;
+using AnimalWeb.Models;
+using AnimalWeb.Tests.FakeRepositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalWeb.Tests.Controllers
+{
+    [TestClass]
+    public class CategoriesControllerTest
+    {
+        [TestMethod]
+        public void ShowCategories()
+        {
+            var categoriesRepository = new FakeMyRepository();
+            var categoriesController = new CategoriesController(categoriesRepository);
+
+            var result = categoriesController.AllCategories() as ViewResult;
+
+            Assert.AreEqual(typeof(List<Categories>), result!.Model!.GetType());
+        }
+
+        [TestMethod]
+        public void ShowAnimals()
+        {
+            var animalsRepository = new FakeMyRepository();
+            var categoriesController = new CategoriesController(animalsRepository);
+
+            var result = categoriesController.GetCategory("Dogs") as ViewResult;
+
+            Assert.AreEqual(typeof(List<Animals>), result!.Model!.GetType());
+        }
+        [TestMethod]
+        public void AnimalPerformance_Pin()
+        {
+            DateTime dt = DateTime.Now;
+            var animalsRepository = new FakeMyRepository();
+            var categoriesController = new CategoriesController(animalsRepository);
+
+            var result = categoriesController.GetCommentsForAnimal(1, false);
+            var timeSpend = DateTime.Now - dt;
+            Assert.IsTrue(timeSpend < TimeSpan.FromSeconds(0.5));
+        }
+
+
+    }
+}
--- AnimalWeb.Tests/FakeRepositories/FakeMyRepository.cs
+++ AnimalWeb.Tests/FakeRepositories/FakeMyRepository.cs
@@ -57,7 +57,13 @@ namespace AnimalWeb.Tests.FakeRepositories
 
         IEnumerable<Animals> IRepository.GetAnimalsByCategory(string category)
         {
-            throw new NotImplementedException();
+            IEnumerable<Animals> animals = new List<Animals>
+            {
+            new Animals { ID = 1, Name = "Rex", Age = 1, PictureName = @"\Assets\Pets\Dogs\Rex.jpg", Description = "Rex is a very friendly dog, he likes to play with ball and really love toys", CategoryName = "Dogs", Price = 120 },
+            new Animals { ID = 2, Name = "Mickey", Age = 2, PictureName = @"\Assets\Pets\Dogs\Miki.jpg", Description = "Mickey is a dog full of energy, happy and bouncy who will always be happy to play with you", CategoryName = "Dogs", Price = 93 },
+            new Animals { ID = 3, Name = "Snow", Age = 3, PictureName = @"\Assets\Pets\Cats\Snow.jpg", Description = "Calm and always loves cuddling", CategoryName = "Cats", Price = 11 }
+            };
+            return animals;
         }
 
         IEnumerable<Animals> IRepository.GetBestAnimals()
@@ -67,7 +73,12 @@ namespace AnimalWeb.Tests.FakeRepositories
 
         IEnumerable<Categories> IRepository.GetCategories()
         {
-            throw new NotImplementedException();
+            IEnumerable<Categories> categories = new List<Categories>
+            {
+            new Categories { ID = 1, Name = "Dogs", CategoryPicture = @"\Assets\CategoriesI-icons\dog-icon.webp" },
+            new Categories { ID = 2, Name = "Cats", CategoryPicture = @"\Assets\CategoriesI-icons\cat-icon.webp" }
+            };
+            return categories;
         }
 
         string IRepository.GetCategoryById(int Id)
@@ -77,7 +88,17 @@ namespace AnimalWeb.Tests.FakeRepositories
 
         IEnumerable<Comments> IRepository.GetCommentsById(int Id)
         {
-            throw new NotImplementedException();
+            IEnumerable<Comments> comments = new List<Comments>
+            {
+                new Comments { ID = 1, Comment = "I will buy that dog!", AnimalID = 1, CommentWriterName = "shir", CommentDate = DateTime.Now },
+                new Comments { ID = 2, Comment = "What a cuty", AnimalID = 1, CommentWriterName = "david", CommentDate = DateTime.Now },
+                new Comments { ID = 3, Comment = "I want one", AnimalID = 1, CommentWriterName = "omer", CommentDate = DateTime.Now },
+                new Comments { ID = 4, Comment = "Lets buy it", AnimalID = 1, CommentWriterName = "sapir", CommentDate = DateTime.Now },
+                new Comments { ID = 5, Comment = "What a cuty", AnimalID = 2, CommentWriterName = "david", CommentDate = DateTime.Now },
+                new Comments { ID = 6, Comment = "I want one", AnimalID = 2, CommentWriterName = "omer", CommentDate = DateTime.Now }
+            };
+
+            return comments;
         }
 
         void IRepository.UpdateAnimal(string name, int age, int price, string description, string pictureURL, int id)
